d4900f394fc8a474b613eedb47255dccf0895b06
Santiago Conelli <santiconelli@hotmail.com>
2020-08-08 19:38:45 -0300

    agregue Id al visualizador de insumos

--- src/main/java/gui/PanelInsumoVisualizarInsumos.java
+++ src/main/java/gui/PanelInsumoVisualizarInsumos.java
@@ -27,7 +27,7 @@ public class PanelInsumoVisualizarInsumos extends JPanel {
 		
 		this.setBackground(Color.orange);
 		
-		
+		this.modeloTablaAtributos.addColumn("Id");
 		this.modeloTablaAtributos.addColumn("Descripcion");
 		this.modeloTablaAtributos.addColumn("Unidad de Medida");
 		this.modeloTablaAtributos.addColumn("Costo");
@@ -35,9 +35,9 @@ public class PanelInsumoVisualizarInsumos extends JPanel {
 		this.modeloTablaAtributos.addColumn("Densidad");
 		this.modeloTablaAtributos.addColumn("Stock");
 		
-		String[] p1 = {"Carton duro","cm3","13","50","","10"};
-		String[] p2 = {"Tornillo Phillips","cm","5","3","","600"};
-		String[] p3 = {"Tornillo tipo Allen","cm","6","4","","465"};
+		String[] p1 = {"1","Carton duro","cm3","13","50","","10"};
+		String[] p2 = {"2","Tornillo Phillips","cm","5","3","","600"};
+		String[] p3 = {"3","Tornillo tipo Allen","cm","6","4","","465"};
 		
 		this.modeloTablaAtributos.addRow(p1);
 		this.modeloTablaAtributos.addRow(p2);
